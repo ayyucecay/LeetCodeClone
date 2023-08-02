8046d1a2d55b56f9e66cbf79e9275dfdcc9947cb
Mer Ay <meray@Mers-MacBook-Pro.local>
2023-08-02 17:42:50 +0300

    finished authorization

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,40 @@
+import { authModalState } from '@/atoms/authModalAtoms';
+import AuthModal from '@/components/Modals/AuthModal';
+import Navbar from '@/components/Navbar/Navbar';
+import { auth } from '@/firebase/firebase';
+import React, {useEffect, useState} from 'react';
+import { useRecoilValue } from 'recoil';
+import {useAuthState} from 'react-firebase-hooks/auth'
+import {useRouter} from 'next/router'
+import Image from 'next/image';
+
+type AuthPageProps = {};
+
+const AuthPage:React.FC<AuthPageProps> = () => {
+    
+    const authModal = useRecoilValue(authModalState)
+
+    const [user, loading, error] = useAuthState(auth)
+    const [pageLoading, setPageLoading] = useState(true)
+    const router = useRouter()
+
+    useEffect(()=> {
+        if(user) router.push('/')
+        if(!loading && !user) setPageLoading(false)
+    }, [user, router, loading])
+
+    if(pageLoading) return null;
+
+    return <div className='bg-gradient-to-b from-gray-600 to to-black h-screen relative'>
+        <div className='max-w-7xl mx-auto'>
+            <Navbar/>
+            <div className='flex items-center justify-center h-[calc(100vh-5rem)] pointer-event-none select-none'>
+              <Image src='/hero.png' alt='Hero img' width={700} height={700}/>
+            </div>
+            {authModal.isOpen && <AuthModal/>}
+        </div>
+        
+    
+    </div>
+}
+export default AuthPage;
